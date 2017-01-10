60a7e9a83cf3c3a24b89438ae3aee748ba4516e6
Connor <connordear@gmail.com>
2017-01-10 16:16:45 -0500

    created placeholder file for problem 15.

---
+++ c solutions/ProjectEuler15.c
@@ -0,0 +1,13 @@
+// Starting in the top left corner of a 2x2 grid, and only being able to move
+// to the right and down, there are exactly 6 routes to the bottom right corner.// How many such routes are there through a 20x20 grid?
+
+int main(void)
+{
+	// Create 20x20 array with each slot filled with a pointer pointing to
+	// the slot underneath it. If it's pointing to nothing then it points
+	// right. If the pointer points to a node beneath it that is pointing
+	// to the right, then on the next iteration it too will point to the
+	// right.
+
+	return 0;	
+}
